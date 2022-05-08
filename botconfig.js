b99d4c2593125a2c7aace1ec4911abc64e38c919
Enrique-Madrid <marlonenrique100@gmail.com>
2022-05-08 05:13:21 -0600

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,27 +1,26 @@
 module.exports = {
-  Admins: ["567177086041587712"], //Admins of the bot
-  ExpressServer: true, //If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || "as!", //Default prefix, Server Admins can change the prefix
-  Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "ODU4MTgyMjUzMjQ5OTUzODI0.YNaagQ.lbCGV0oUNAfi631y7_CWWKJcLGE", //Discord Bot Token
+  Admins: ["567177086041587712"], //Admin del Bot
+  ExpressServer: true, //Si quieres correr el website
+  DefaultPrefix: process.env.Prefix || "as!", //Prefix predeterminado
+  Port: 3000, //Puerto
+  SupportServer: "",
+  Token: process.env.Token || "Token", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "858182253249953824", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "r4cw5OcHZRXiTOIU4mMHul253MTYpFaS", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
-  ServerDeafen: true, //If you want bot to stay deafened
-  DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
-  CallbackURL: "/api/callback", //Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
-  "24/7": true, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Astolfo mi dios uwu", //A Secret like a password
-  IconURL:
-    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
-  EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
-  Permissions: 2205281600, //Bot Inviting Permissions
-  Website: process.env.Website || "localhost", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
+  ServerDeafen: true, //Si quieres que el bot esté ensordecido
+  DefaultVolume: 100, //Volumen por default
+  CallbackURL: "/api/callback", //Discord API Callback
+  "24/7": true, //Si quieres qué el bot esté 24/7
+  CookieSecret: "Astolfo mi dios uwu",
+ 
+  EmbedColor: "RANDOM", 
+  Permissions: 2205281600,
+  Website: process.env.Website || "localhost",
   
   Presence: {
-    status: "online", // You can show online, idle, and dnd
-    name: "Music", // The message shown
+    status: "online", // Para usar el online, idle, and dnd
+    name: "Music", //
     type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
@@ -29,12 +28,12 @@ module.exports = {
   Lavalink: {
     id: "Main",
     host: "lava.link",
-    port: 80, // The port that lavalink is listening to. This must be a number!
+    port: 80, 
     pass: "youshallnotpass",
-    secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
+    secure: false,
   },
 
-  //Please go to https://developer.spotify.com/dashboard/
+  //Ir a https://developer.spotify.com/dashboard/
   Spotify: {
     ClientID: process.env.Spotify_ClientID || "c853a10a7c2c472d90d30b4a8db1f5b4", //Spotify Client ID
     ClientSecret: process.env.Spotify_ClientSecret || "37027fc838594797bfeff8eec62ce351", //Spotify Client Secret
